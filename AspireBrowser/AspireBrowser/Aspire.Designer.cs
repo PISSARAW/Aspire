a924118ac5821771365a8e5fe27c5570a8e68a5c
Ginks <pissabruney@hotmail.com>
2017-05-12 13:17:07 +0200

    Navigateur Web

---
+++ AspireBrowser/AspireBrowser/Aspire.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace AspireBrowser
+{
+    partial class Aspires
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Aspires));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nouvelleFenêtreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nouvelleFenêtrePrivéeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historiqueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.téléchargementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plusDoutilsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionnaireDeTâchesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.outilsDeDéveloppementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paramètresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quiterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.search = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(37, 434);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "📷";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 19);
+            this.toolStripMenuItem1.Text = "➕";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(24, 19);
+            this.toolStripMenuItem2.Text = "▶";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(24, 19);
+            this.toolStripMenuItem3.Text = "📷";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(24, 19);
+            this.toolStripMenuItem4.Text = "🌅";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(24, 19);
+            this.toolStripMenuItem5.Text = "📥";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nouvelleFenêtreToolStripMenuItem,
+            this.nouvelleFenêtrePrivéeToolStripMenuItem,
+            this.historiqueToolStripMenuItem,
+            this.téléchargementToolStripMenuItem,
+            this.plusDoutilsToolStripMenuItem,
+            this.paramètresToolStripMenuItem,
+            this.quiterToolStripMenuItem});
+            this.toolStripMenuItem6.Margin = new System.Windows.Forms.Padding(0, 315, 0, 0);
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(24, 19);
+            this.toolStripMenuItem6.Text = "🔧";
+            // 
+            // nouvelleFenêtreToolStripMenuItem
+            // 
+            this.nouvelleFenêtreToolStripMenuItem.Name = "nouvelleFenêtreToolStripMenuItem";
+            this.nouvelleFenêtreToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.nouvelleFenêtreToolStripMenuItem.Text = "Nouvelle Fenêtre";
+            // 
+            // nouvelleFenêtrePrivéeToolStripMenuItem
+            // 
+            this.nouvelleFenêtrePrivéeToolStripMenuItem.Name = "nouvelleFenêtrePrivéeToolStripMenuItem";
+            this.nouvelleFenêtrePrivéeToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.nouvelleFenêtrePrivéeToolStripMenuItem.Text = "Nouvelle Fenêtre Privée";
+            // 
+            // historiqueToolStripMenuItem
+            // 
+            this.historiqueToolStripMenuItem.Name = "historiqueToolStripMenuItem";
+            this.historiqueToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.historiqueToolStripMenuItem.Text = "Historique";
+            // 
+            // téléchargementToolStripMenuItem
+            // 
+            this.téléchargementToolStripMenuItem.Name = "téléchargementToolStripMenuItem";
+            this.téléchargementToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.téléchargementToolStripMenuItem.Text = "Téléchargement";
+            // 
+            // plusDoutilsToolStripMenuItem
+            // 
+            this.plusDoutilsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionnaireDeTâchesToolStripMenuItem,
+            this.outilsDeDéveloppementToolStripMenuItem});
+            this.plusDoutilsToolStripMenuItem.Name = "plusDoutilsToolStripMenuItem";
+            this.plusDoutilsToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.plusDoutilsToolStripMenuItem.Text = "Plus d\'outils";
+            // 
+            // gestionnaireDeTâchesToolStripMenuItem
+            // 
+            this.gestionnaireDeTâchesToolStripMenuItem.Name = "gestionnaireDeTâchesToolStripMenuItem";
+            this.gestionnaireDeTâchesToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.gestionnaireDeTâchesToolStripMenuItem.Text = "Gestionnaire de tâches";
+            // 
+            // outilsDeDéveloppementToolStripMenuItem
+            // 
+            this.outilsDeDéveloppementToolStripMenuItem.Name = "outilsDeDéveloppementToolStripMenuItem";
+            this.outilsDeDéveloppementToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.outilsDeDéveloppementToolStripMenuItem.Text = "Outils de développement";
+            // 
+            // paramètresToolStripMenuItem
+            // 
+            this.paramètresToolStripMenuItem.Name = "paramètresToolStripMenuItem";
+            this.paramètresToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.paramètresToolStripMenuItem.Text = "Paramètres";
+            // 
+            // quiterToolStripMenuItem
+            // 
+            this.quiterToolStripMenuItem.Name = "quiterToolStripMenuItem";
+            this.quiterToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.quiterToolStripMenuItem.Text = "Quiter";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(203, 59);
+            this.search.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(436, 20);
+            this.search.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(833, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(24, 434);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(21, 20);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(21, 20);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(21, 20);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(21, 20);
+            this.toolStripButton4.Text = "toolStripButton4";
+            // 
+            // Aspires
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(857, 434);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Aspires";
+            this.Text = "AspireBrowser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem nouvelleFenêtreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nouvelleFenêtrePrivéeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historiqueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem téléchargementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem plusDoutilsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionnaireDeTâchesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outilsDeDéveloppementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paramètresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quiterToolStripMenuItem;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+    }
+}
+
